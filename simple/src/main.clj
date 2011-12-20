69be7e46780038f5fc34df3c7257fa0a44f6369d
Edd Dumbill <edd@me.com>
2011-12-19 17:08:54 -0800

    Tidying up

--- simple/src/main.clj
+++
@@ -1,9 +0,0 @@
-(ns coin-tosser.main
-  (:use [coin-tosser.core :only [chart-scores sample-scores]])
-  (:gen-class))
-
-(defn -main
-  "Main entry point"
-  [repetitions sample-size]
-  (chart-scores (sample-scores (Integer/parseInt repetitions)
-                               (Integer/parseInt sample-size))))
